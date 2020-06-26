43f19587c30650ccfcdf2dcac822bdf9e6a230f4
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-06-26 18:03:37 +0200

    editing signIn form

--- src/component/signin/Signin.js
+++ src/component/signin/Signin.js
@@ -17,24 +17,23 @@ class signin extends Component {
     }
     onSubmitSignIn = () => {
         fetch('http://localhost:3000/signin', {
-            method :'post',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify({
-                email: this.state.signInEmail,
-                password:this.state.signInPassword
-            })
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            email: this.state.signInEmail,
+            password: this.state.signInPassword
+          })
         })
-            .then(response => response.json())
-            .then(data => {
-                if (data ==='success') {
-                    this.props.onRouteChange('home');
-                }
-            })
-        
-    }
+          .then(response => response.json())
+          .then(user => {
+            if (user.id) {
+                this.props.loadUser(user)
+                this.props.onRouteChange('home');
+            }
+          })
+      }
 
     render() {
-        const {onRouteChange} = this.props;
         return (
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
@@ -66,15 +65,14 @@ class signin extends Component {
                     <div className="">
             
                     <input 
-                        onClick={this.onSubmitSignIn}
+                        onClick={this.props.onRouteChange('home')}
                         className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                         type="submit" 
-                        value="Sign in" 
-
+                        value="Sign in"
                     />
                     </div>
                     <div className="lh-copy mt3">
-                    <p onClick={() => onRouteChange('Register')} className="f6 link dim black db pointer">Register</p>
+                    <p onClick={() => this.props.onRouteChange('Register')} className="f6 link dim black db pointer">Register</p>
                     </div>
                 </div>
             </main>
