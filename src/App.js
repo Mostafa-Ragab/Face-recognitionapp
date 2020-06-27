08eb28932b32b017bf92a8936a1dbfce57e0c99d
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-06-27 16:45:21 +0200

    making initialstate to signOut

--- src/App.js
+++ src/App.js
@@ -27,25 +27,25 @@ const particlesOptions = {
     }
   }
 }
-
+const initialstate = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name:'',
+    email:'',
+    password: '',
+    entries: 0,
+    joined: ''
+  }
+}
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name:'',
-        email:'',
-        password: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialstate;
   }
 
   loadUser = (data) => {
@@ -107,7 +107,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialstate)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
