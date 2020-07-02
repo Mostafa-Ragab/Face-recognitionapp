afea11b4e4edb32e473c73911199bb89bbc631eb
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-07-02 12:28:45 +0200

    making a validate to sing in

--- src/component/signin/Signin.js
+++ src/component/signin/Signin.js
@@ -68,7 +68,7 @@ class signin extends Component {
                         onClick={this.props.onRouteChange('home')}
                         className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                         type="submit" 
-                        value="Sign in"
+                        value="Sign In"
                     />
                     </div>
                     <div className="lh-copy mt3">
