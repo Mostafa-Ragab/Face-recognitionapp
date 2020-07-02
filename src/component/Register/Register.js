c92d2b380d8e5fa6837b8929b82bac919ccba9c6
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-07-02 12:29:28 +0200

    adding a validation to register form

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -19,25 +19,26 @@ class Register extends Component {
     onPasswordChange = (event) => {
         this.setState({password:event.target.value})
     }
+
     onSubmitSignIn = () => {
         fetch('http://localhost:3000/register', {
-            method :'post',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify({
-                email: this.state.email,
-                password: this.state.password,
-                name: this.state.name
-            })
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            email: this.state.email,
+            password: this.state.password,
+            name: this.state.name
+          })
         })
-            .then(response => response.json())
-            .then(user => {
-                if (user.id) {
-                    this.props.loadUser(user)
-                    this.props.onRouteChange('home');
-                }
-            })
-        
-    }
+          .then(response => response.json())
+          .then(user => {
+            if (user.id) {
+                this.props.loadUser(user)
+                this.props.onRouteChange('home');
+            }
+          })
+      }
+
     render () {
         return (
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -77,13 +78,12 @@ class Register extends Component {
                         </div>
                         </fieldset>
                         <div className="">
-                
+
                         <input 
-                            onClick={this.onSubmitSignIn}
+                            onClick={this.props.onRouteChange('home')}
                             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                             type="submit" 
-                            value="Register" 
-
+                            value="reg"
                         />
                         </div>
                     </div>
