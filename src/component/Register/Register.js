16f8ff9c8d0acb706a04b69a33f7b9dfa857110e
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-07-03 15:48:34 +0200

    response with user.id

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -83,7 +83,7 @@ class Register extends Component {
                             onClick={this.props.onRouteChange('home')}
                             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                             type="submit" 
-                            value="reg"
+                            value="register"
                         />
                         </div>
                     </div>
