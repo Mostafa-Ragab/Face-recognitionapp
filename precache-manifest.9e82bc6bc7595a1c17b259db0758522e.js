babc36d2a59e596a3ceabfce37ae0fb4ae45808a
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-26 16:28:41 +0200

    Updates

---
+++ precache-manifest.9e82bc6bc7595a1c17b259db0758522e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6d2117a2221219d3fdc24cf7029be2",
+    "url": "/Face-recognitionapp/index.html"
+  },
+  {
+    "revision": "7787584fb991d214c842",
+    "url": "/Face-recognitionapp/static/css/2.7a3f122f.chunk.css"
+  },
+  {
+    "revision": "dac518472be9439180b7",
+    "url": "/Face-recognitionapp/static/css/main.423a1c60.chunk.css"
+  },
+  {
+    "revision": "7787584fb991d214c842",
+    "url": "/Face-recognitionapp/static/js/2.3e8c0687.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-recognitionapp/static/js/2.3e8c0687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac518472be9439180b7",
+    "url": "/Face-recognitionapp/static/js/main.7828646e.chunk.js"
+  },
+  {
+    "revision": "87229629403742303a3d",
+    "url": "/Face-recognitionapp/static/js/runtime-main.f88bd544.js"
+  }
+]);
